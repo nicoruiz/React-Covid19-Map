106ca6a84abc507a21ce51423ad93455a3189ce4
Nicolas Ruiz <nicoruiz182@gmail.com>
2020-04-19 22:32:17 -0300

    Load more feature added. Now dashboard starts showing 10 countries.

--- src/components/dashboard/CountriesList.js
+++ src/components/dashboard/CountriesList.js
@@ -3,6 +3,7 @@ import { withStyles } from "@material-ui/core/styles";
 import CountryCard from "components/dashboard/CountryCard";
 import { Grid } from "@material-ui/core";
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 import * as countriesProvider from "../../providers/countries.js";
 import Spinner from "../Spinner.js";
 
@@ -11,13 +12,17 @@ const useStyles = () => ({
 		flexGrow: 1,
 		marginLeft: 30,
 		marginRight: 30
+	},
+	loadMore: {
+		marginTop: 10
 	}
 });
 
 class CountriesList extends React.Component {
 	state = {
 		countries: [],
-		filtered: []
+		filtered: [],
+		visible: 10
 	}
 
 	componentDidMount() {
@@ -37,8 +42,14 @@ class CountriesList extends React.Component {
 		let value = event.target.value;
 		this.setState({
 			filtered: this.state.countries
-									.filter(c => c.country.toLowerCase()
-									.includes(value.toLowerCase()))
+				.filter(c => c.country.toLowerCase()
+					.includes(value.toLowerCase()))
+		});
+	}
+
+	loadMore = () => {
+		this.setState({
+			visible: this.state.visible + 10
 		});
 	}
 
@@ -52,15 +63,19 @@ class CountriesList extends React.Component {
 					<div className={classes.root}>
 						<Grid container spacing={3}>
 							<Grid noValidate autoComplete="off" item>
-								<TextField id="standard-basic" label="Search" onChange={(event) => this.onSearch(event)}/>
+								<TextField id="standard-basic" label="Search" onChange={(event) => this.onSearch(event)} />
 							</Grid>
 							{this.state.filtered
 								.sort((x, y) => y.cases - x.cases)
-								.map(c => (
-									<Grid key={c.country} item xs={12}>
+								.slice(0, this.state.visible)
+								.map((c, index) => (
+									<Grid key={index} item xs={12}>
 										<CountryCard country={c} />
 									</Grid>
 								))}
+								<Button variant="contained" color="primary" onClick={this.loadMore} className={classes.root}>
+									Load More
+								</Button>
 						</Grid>
 					</div>
 				}
