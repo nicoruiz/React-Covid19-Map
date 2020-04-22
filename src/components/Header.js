ec7d8a0ea27d79991cd289e50d13e52cdfc5f4be
Nicolás Ruiz <Miguel.Nicolas.Ruiz@ey.com>
2020-04-22 01:58:01 -0300

    Icons added to nav buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Link, navigate } from "gatsby";
-
-
+import { Link } from "gatsby";
 import { makeStyles } from "@material-ui/core/styles";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import Button from "@material-ui/core/Button";
+import PublicIcon from '@material-ui/icons/Public';
+import ViewListIcon from '@material-ui/icons/ViewList';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -30,10 +30,10 @@ const Header = () => {
           <Typography variant="h6" className={classes.title}>
             Coronavirus (COVID-19)
           </Typography>
-          <Button color="inherit">
+          <Button color="inherit" startIcon={<PublicIcon />} className={classes.menuButton}>
             <Link to="/">Map</Link>
           </Button>
-          <Button color="inherit">
+          <Button color="inherit" startIcon={<ViewListIcon />}>
             <Link to="/dashboard/">Dashboard</Link>
           </Button>
         </Toolbar>
